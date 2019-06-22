3eb8831fd18f184894549e4f817597674995efa1
Andrew Davisson <akdavisson4@gmail.com>
2019-06-22 15:15:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,8 @@ class Student
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade INTEGER);
+        grade INTEGER)
+      SQL
   end
   
 end
